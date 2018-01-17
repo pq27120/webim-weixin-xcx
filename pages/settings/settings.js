06a1b9caaaa2f5f06c9f19ee7d6ad3b4ac243c0a
pq27120 <pq27120@126.com>
2018-01-17 23:40:39 +0800

    init commit

---
+++ pages/settings/settings.js
@@ -0,0 +1,45 @@
+var strophe = require('../../utils/strophe.js')
+var WebIM = require('../../utils/WebIM.js')
+var WebIM = WebIM.default
+
+Page({
+	data: {
+		username:''
+	},
+	onLoad: function() {
+		var myUsername = wx.getStorageSync('myUsername')
+		this.setData({
+			username: myUsername
+		})
+	},
+	tab_contact: function() {
+		wx.redirectTo({
+			url: '../main/main'
+		})
+	},
+	tab_chat: function() {
+		wx.redirectTo({
+			url: '../chat/chat'
+		})
+	},
+	person: function() {
+		var myUsername = wx.getStorageSync('myUsername')
+		wx.navigateTo({
+			url: '../friend_info/friend_info?yourname=' + myUsername
+		})
+	},
+	logout: function() {
+		wx.showModal({
+			title: '是否退出登录',
+			success: function(res) {
+			    if (res.confirm) {
+		   	        WebIM.conn.close()
+		   	        //wx.closeSocket()
+		   	        wx.redirectTo({
+		   	        	url: '../login/login'
+		   	        })
+				}
+			}
+		})
+	}
+})
