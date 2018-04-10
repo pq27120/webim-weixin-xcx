0940d72e7e9227de6e1b245505ab87de340e7404
zhuli <zhuli2@aiyoumi.com>
2018-04-10 20:54:25 +0800

    用户如果不选择授权时异常修复

--- pages/register/register.js
+++ pages/register/register.js
@@ -19,10 +19,10 @@ Page({
   onLoad: function (option) {
     this.setData({
       isDoctor: getApp().globalData.role === 1,
-      registername: getApp().globalData.userInfo.nickName,
-      registersex: (getApp().globalData.userInfo.gender == 1) ? '男' : '女'
+      registername: (getApp().globalData.userInfo == null) ? '' : getApp().globalData.userInfo.nickName,
+      registersex: (getApp().globalData.userInfo == null) ? '男' : ((getApp().globalData.userInfo.gender == 1) ? '男' : '女')
     });
-    // console.log('getApp().globalData.role=' + this.data.isDoctor);
+    // console.log('getApp().globalData.userInfo=' + getApp().globalData.userInfo);
   },
   register: function () {
     // console.log('doctor=' + this.data.doctorInfo.name);
@@ -30,18 +30,47 @@ Page({
     if (this.data.isDoctor) {
       this.registerDoctor();
     } else {
-      if (this.checkPatient()) {
+      // if (this.checkPatient()) {
         this.registerPatient();
-      }
+      // }
     }
   },
 
   registerDoctor: function () {
-    // console.log(this.data.doctorInfo.department);
+    // console.log(this.data.registername);
+    console.log(this.data.doctorInfo.department);
     // console.log(this.data.doctorInfo.grade);
-    // console.log(this.data.doctorInfo.hospital);
+    console.log(this.data.doctorInfo.hospital);
     // console.log(4);
     // console.log(this.data.doctorInfo.name);
+    var name = this.data.registername;
+    var sex = this.data.registersex;
+
+    if (this.data.registername == '') {
+      wx.showToast({
+        title: '请填写医生姓名',
+        duration: 2000
+      })
+      return;
+    }
+    // console('name=' + name);
+
+    if (this.data.doctorInfo.hospital == undefined){
+      wx.showToast({
+        title: '请选择所在医院',
+        duration: 2000
+      })
+      return;
+    }
+
+    if (this.data.doctorInfo.department == undefined){
+      wx.showToast({
+        title: '请选择所在科室',
+        duration: 2000
+      })
+      return;
+    }
+    var name = this.data.registername;
     wx.request({
       // url: getApp().globalData.api.doctorLogin + "?wechatId=" + getApp().globalData.userInfo.nickName,
       url: getApp().globalData.api.doctorLogin + "?wechatId=" + getApp().globalData.unionId,
@@ -51,7 +80,7 @@ Page({
         "hospitalId": this.data.doctorInfo.hospital,
         "monthPrice": 0,
         // "name": this.data.doctorInfo.name
-        "name": getApp().globalData.userInfo.nickName
+        "name": name
       },
       header: { 'Content-Type': "application/json" },
       method: 'post',
@@ -64,11 +93,37 @@ Page({
   },
 
   registerPatient: function (e) {
+    // return;
     // console.log('>>>' + getApp().globalData.userInfo.nickName);
     // console.log('>>>' + this.data.patientInfo.tel);
-    // console.log('>>>' + this.data.patientInfo.birthday);
+    // console.log('>>>' + this.data.patientInfo.birthday);return;
     // console.log('>>>' + getApp().globalData.userInfo.gender);
-    // console.log(e.detail.value);
+    // console.log('>>>>>>>register= ' + this.data.patientInfo);
+    // console.log('>>>>>>>register= ' + this.data.patientInfo.name);
+    // console.log('>>>>>>>register= ' + (this.data.patientInfo.name == null));
+    var name = this.data.registername;
+    var sex = this.data.registersex;
+
+    if (this.data.registername == '') {
+      wx.showToast({
+        title: '请填写患者姓名',
+        duration: 2000
+      })
+      return;
+    }
+
+    if (!this.checkPatient()){
+      return;
+    }
+
+    if (this.data.patientInfo.birthday == undefined){
+      wx.showToast({
+        title: '请选择患者生日',
+        duration: 2000
+      })
+      return;
+    }
+    
     wx.request({
       // url: getApp().globalData.api.patientLogin + "?wechatId=" + getApp().globalData.userInfo.nickName,
       url: getApp().globalData.api.patientLogin + "?wechatId=" + getApp().globalData.unionId,
@@ -76,9 +131,12 @@ Page({
         "birthday": this.data.patientInfo.birthday,
         "mobile": this.data.patientInfo.tel,
         // "name": this.data.patientInfo.name,
-        "name": getApp().globalData.userInfo.nickName,
+        // "name": getApp().globalData.userInfo.nickName,
+        "name": name,
+        "sex": sex
         // "sex": (this.data.patientInfo.sex == '男') ? '1' : '0'
-        "sex": getApp().globalData.userInfo.gender
+        // "sex": getApp().globalData.userInfo.gender
+
       },
       header: { 'Content-Type': "application/json" },
       method: 'post',
@@ -91,6 +149,11 @@ Page({
   },
 
   checkPatient: function () {
+    var userinfo = getApp().globalData.userInfo;
+    if (userinfo) {
+
+    }
+
     var telregular = /^\d{11}$/;
     var tel = this.data.patientInfo.tel;
     // console.log('tel=' + tel);
@@ -109,7 +172,8 @@ Page({
 
   bindDoctorName: function (e) {
     this.setData({
-      'doctorInfo.name': e.detail.value
+      'doctorInfo.name': e.detail.value,
+      registername: e.detail.value
     })
   },
   bindDoctorHospital: function (e) {
@@ -137,13 +201,16 @@ Page({
   // 患者信息
   bindPatientName: function (e) {
     this.setData({
-      'patientInfo.name': e.detail.value
+      'patientInfo.name': e.detail.value,
+      registername : e.detail.value
     })
     // console.log(this.data.patientInfo)
   },
+
   bindPatientSex: function (e) {
     this.setData({
-      'patientInfo.sex': e.detail.value
+      'patientInfo.sex': e.detail.value,
+      registersex : e.detail.value == '男' ? e.detail.value : '女'
     })
 
   },
